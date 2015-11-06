7d26f9a9e81a9b72fbb0f41b7e596b47b6481a30
Anthony Watanabe <atwatanabe314@gmail.com>
2015-11-06 15:52:18 -0800

    Dots/circles now show up right at the tip of the cursor, although my solution is hard-coded, and it doesn't work if the canvas is resized yet.

--- src/main/FrontEnd/sessionScripts.js
+++ src/main/FrontEnd/sessionScripts.js
@@ -7,9 +7,12 @@ ctx.fillRect(0, 0, c.width, c.height);
 
 function myFunction(event)
 {
+	var x = event.pageX - c.offsetLeft - 5;	//5: border width
+	var y = event.pageY - c.offsetTop - 5;
+	//alert(x + ' ' + y);
+	
 	ctx.beginPath();
-	ctx.arc(event.x / 3, event.y / 3, 1, 0, 2 * Math.PI);
+	ctx.arc(x, y, 1, 0, 2 * Math.PI);
 	//need to map event coordinates to canvas coordinates
 	ctx.stroke();
 }
-
