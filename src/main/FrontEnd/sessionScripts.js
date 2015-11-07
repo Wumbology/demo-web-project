787f7fd0b94b3e7fa5d74f3f060686d0ab351fc8
Anthony Watanabe <atwatanabe314@gmail.com>
2015-11-06 18:06:34 -0800

    Added a brush size slider bar.

--- src/main/FrontEnd/sessionScripts.js
+++ src/main/FrontEnd/sessionScripts.js
@@ -5,9 +5,13 @@ ctx.fillStyle = "#FFFFFF";
 ctx.fillRect(0, 0, c.width, c.height);
 ctx.canvas.width = window.innerWidth;
 ctx.canvas.height = window.innerHeight;
+//need to load canvas state
+
+var currentX, currentY;	//draw line/curve between points recorded by mouseMove
+						//to get continuous drawing instead of individual circles
 
 
-var brushSize = 5;	//get this from a setting
+var brushSize = 1;	//get this from a setting
 
 var border = 5;
 
@@ -21,6 +25,7 @@ function mouseClick(event)
 function mouseDown(event)
 {
 	_mouseDown = true;
+	
 	draw(calcX(event.pageX), calcY(event.pageY), brushSize);
 }
 
@@ -55,4 +60,9 @@ function calcX(rawX)
 function calcY(rawY)
 {
 	return rawY - c.offsetTop - border;
+}
+
+function changeBrushSize(size)
+{
+	brushSize = size;
 }
