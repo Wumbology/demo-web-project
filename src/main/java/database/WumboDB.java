657c53b16ae9ac236e0078e3072682b2a2214f8e
Jonathan Francisco <jdavidfrancisco@gmail.com>
2015-11-29 22:32:28 -0800

    updated database, groups table created and holds the groupnames for each group a user is using

--- src/main/java/database/WumboDB.java
+++ src/main/java/database/WumboDB.java
@@ -103,4 +103,52 @@ public class WumboDB {
 		 
 		 return ans;
 	 }
+	 
+	 public static void addGroup(String groupName) {
+	 Connection conn = null;
+	 Statement stmt = null;
+	 try{
+	    //STEP 2: Register JDBC driver
+	    Class.forName("com.mysql.jdbc.Driver");
+
+	    //STEP 3: Open a connection
+	    System.out.println("Connecting to a selected database...");
+	    conn = DriverManager.getConnection(DB_URL, USER, PASS);
+	    System.out.println("Connected database successfully...");
+	    
+	    //STEP 4: Execute a query
+	    String queryCheck = "SELECT * from User WHERE username = '" + groupName + "';";
+	    ResultSet rs = stmt.executeQuery(queryCheck);
+	    if (rs.next()) {
+	    	System.out.println("username already exists");
+	    }
+	    System.out.println("Inserting records into the table...");
+	    stmt = conn.createStatement();
+	    
+	    String sql = "INSERT INTO Groups " +
+                "VALUES ('" + groupName + "';)";
+	    stmt.executeUpdate(sql);
+	    System.out.println("Inserted records into the table...");
+
+	 }catch(SQLException se){
+	    //Handle errors for JDBC
+	    se.printStackTrace();
+	 }catch(Exception e){
+	    //Handle errors for Class.forName
+	    e.printStackTrace();
+	 }finally{
+	    //finally block used to close resources
+	    try{
+	       if(stmt!=null)
+	          conn.close();
+	    }catch(SQLException se){
+	    }// do nothing
+	    try{
+	       if(conn!=null)
+	          conn.close();
+	    }catch(SQLException se){
+	       se.printStackTrace();
+	    }//end finally try
+	 }//end try
+	}
 }
