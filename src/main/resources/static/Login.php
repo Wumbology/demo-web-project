d86438c2025a99805d98bde6e4d85eab13151e0d
Juan Gonzalez <juanegonzalez1@hotmail.com>
2015-11-30 19:54:41 -0800

    Create Login.php

---
+++ src/main/resources/static/Login.php
@@ -0,0 +1,33 @@
+<?php
+
+$host="wumbo-db-instance.cw661l5aptwb.us-west-2.rds.amazonaws.com:3306/wumboideas_database"; // Host name 
+$username="wumbology"; // Mysql username 
+$password="loyalty1"; // Mysql password 
+$db_name="wumboideas_database"; // Database name 
+$tbl_name="members"; // Table name 
+
+// Connect to server and select databse.
+mysql_connect("$host", "$username", "$password")or die("cannot connect"); 
+mysql_select_db("$db_name")or die("cannot select DB");
+
+// username and password sent from form 
+$username=$_POST['username']; 
+$password=$_POST['password']; 
+
+// To protect MySQL injection (more detail about MySQL injection)
+$username = stripslashes($username);
+$password = stripslashes($password);
+$username = mysql_real_escape_string($username);
+$password = mysql_real_escape_string($password);
+$sql="SELECT * FROM $tbl_name WHERE username='$username' and password='$password'";
+$result=mysql_query($sql);
+
+// Mysql_num_row is counting table row
+$count=mysql_num_rows($result);
+
+// If result matched $username and $password, table row must be 1 row
+if($count==1){
+    session_start();
+    $_SESSION['loggedin'] = true;
+    $_SESSION['username'] = $username;
+}
